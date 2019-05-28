6aa4acf120186339c0bc004f49ff73b77615ab43
Angie Gomez <angie@Angies-MacBook-Air.local>
2019-05-27 22:01:21 -0700

    table style

--- static/js/line.js
+++ static/js/line.js
@@ -36,7 +36,11 @@ d3.json("/staples_crimes").then(function (data) {
     var trace1 = {
         x: staples_data.x,
         y: staples_data.y,
-        type: "scatter"
+        type: "scatter",
+        line: {
+            color: "#DC143C"
+          },
+        name: "Staples Center"
        };
     d3.json("/coliseum_crimes").then(function (data) {
         var coliseum_data = split_key_values(group_by_distance(data));
@@ -44,21 +48,34 @@ d3.json("/staples_crimes").then(function (data) {
         var trace2 = {
             x: coliseum_data.x,
             y: coliseum_data.y,
-            type: "scatter"
+            type: "scatter",
+            line: {
+                color: "#FFD700"
+              },
+            name: "Coliseum"
            };
-        // var coliseum_crime_frequency = split_key_values(data);
         d3.json("/dodger_crimes").then(function (data) {
             var dodger_data = split_key_values(group_by_distance(data));
             console.log(dodger_data);
             var trace3 = {
                 x: dodger_data.x,
                 y: dodger_data.y,
-                type: "scatter"
+                type: "scatter",
+                line: {
+                    color: "#0000FF"
+                  },
+                name: "Dodger Stadium"
                };
+
+    var layout = {
+        title: "Crime Count vs. Distance, by Venue",
+        xaxis: { title: "Distance (mi)"},
+        yaxis: { title: "Number of Crimes"}
+    };
 
     var line_data = [trace1,trace2,trace3];
 
-    Plotly.newPlot("plot", line_data);
+    Plotly.newPlot("plot", line_data, layout);
         });
     });
 });
